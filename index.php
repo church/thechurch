eef1aa6ddb8e3ddee1a977554b9a37c61aa29b10
David Barratt <david@thechur.ch>
2011-08-27 19:04:06 -0400

    Now put everything back to the way it is supposed to be. After update, Prime will always pull the 0.x branch, as long as we are working on a different branch then what is current, everything should be groovy.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>thechur.ch testing...x2</title>
+		<title>thechur.ch</title>
 		<style type="text/css">
 			@import url('css/style.css');
 		</style>
