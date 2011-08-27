866143487bb3e6642aecbe2cc709827f3dd1e407
David Barratt <david@davidwbarratt.com>
2011-08-27 15:43:02 -0700

    Initial import of existing content.

--- index.php
+++ index.php
@@ -1,21 +1,33 @@
-<?php
-
-/**
- * @file
- * The PHP page that serves all page requests on a Drupal installation.
- *
- * The routines here dispatch control to the appropriate handler, which then
- * prints the appropriate page.
- *
- * All Drupal code is released under the GNU General Public License.
- * See COPYRIGHT.txt and LICENSE.txt.
- */
-
-/**
- * Root directory of Drupal installation.
- */
-define('DRUPAL_ROOT', getcwd());
-
-require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-menu_execute_active_handler();
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>thechur.ch</title>
+		<style type="text/css">
+			@import url('css/style.css');
+		</style>
+		<meta name="viewport" content="width=785" />
+		<script type="text/javascript">
+		
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-24641704-1']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+		</script> 
+	</head>
+	<body>
+		<section>
+			<article>
+				<img src="/img/logo2.png" alt="thechur.ch" />
+				<a href="http://www.facebook.com/thechur.ch">facebook.com/thechur.ch</a>
+				<a href="http://twitter.com/thechur_ch">twitter.com/thechur_ch</a>
+			</article>
+		</section>
+	</body>
+</html>
