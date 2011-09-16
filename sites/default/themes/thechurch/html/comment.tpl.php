de05a353638237999fd0412e6d88b516ced79b47
David Barratt <david@thechur.ch>
2011-09-16 17:13:29 -0400

    update the comment template.

--- sites/default/themes/thechurch/html/comment.tpl.php
+++ sites/default/themes/thechurch/html/comment.tpl.php
@@ -70,7 +70,7 @@
   <div class="content"<?php print $content_attributes; ?>>
     <?php
       // We hide the comments and links now so that we can render them later.
-      hide($content['links']);
+      // hide($content['links']);
       print render($content);
     ?>
     <?php if ($signature): ?>
