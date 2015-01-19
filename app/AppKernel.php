b44d3659d4d13bd2c7344552d4f64522fcd84899
David Barratt <david@davidwbarratt.com>
2015-01-19 00:06:58 -0500

    #31 Upgrade @Symfony to 2.6

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,14 +16,13 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Church\Chruch(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Hip\MandrillBundle\HipMandrillBundle(),
-            new Church\Bundle\UserBundle\ChurchUserBundle(),
-            new Church\Bundle\PlaceBundle\ChurchPlaceBundle(),
-            new Church\Bundle\TeaserBundle\ChurchTeaserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
