67e020fbfddc8fc2f33f62dcab61da68b7acdea9
David Barratt <david@thechur.ch>
2017-03-04 17:55:00 -0500

    Add Tests for Default Controller

---
+++ src/Church/Tests/Controller/ControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Church\Tests\Controller;
+
+use Church\Serializer\SerializerInterface;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+abstract class ControllerTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Gets the mock serializer.
+     */
+    protected function getSerializer()
+    {
+        return $this->createMock(SerializerInterface::class);
+    }
+
+    /**
+     * Gets the mock doctrine.
+     */
+    protected function getDoctrine()
+    {
+        return $this->createMock(RegistryInterface::class);
+    }
+
+    /**
+     * Gets the mock token storage.
+     */
+    protected function getTokenStorage()
+    {
+        return $this->createMock(TokenStorageInterface::class);
+    }
+}
---
+++ src/Church/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Church\Tests\Controller;
+
+use Church\Controller\DefaultController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultControllerTest extends ControllerTest
+{
+
+    /**
+     * Tests the index action.
+     */
+    public function testIndexAction()
+    {
+        $request = new Request();
+        $request->setRequestFormat('json');
+        $data = [
+            'hello' => 'world!',
+        ];
+        $response = new Response(json_encode($data));
+
+        $serializer = $this->getSerializer();
+        $serializer->expects($this->once())
+                   ->method('respond')
+                   ->with($data)
+                   ->willReturn($response);
+
+        $default = new DefaultController($serializer, $this->getDoctrine(), $this->getTokenStorage());
+        $result = $default->indexAction($request);
+
+        $this->assertEquals($response, $result);
+    }
+}
