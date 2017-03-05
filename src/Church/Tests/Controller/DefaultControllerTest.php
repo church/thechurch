94f441408267878e33879c9928ef31e9995ad28c
David Barratt <david@thechur.ch>
2017-03-04 19:38:55 -0500

    Remove all tests

--- src/Church/Tests/Controller/DefaultControllerTest.php
+++ src/Church/Tests/Controller/DefaultControllerTest.php
@@ -2,34 +2,34 @@
 
 namespace Church\Tests\Controller;
 
-use Church\Controller\DefaultController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+// use Church\Controller\DefaultController;
+// use Symfony\Component\HttpFoundation\Request;
+// use Symfony\Component\HttpFoundation\Response;
 
 class DefaultControllerTest extends ControllerTest
 {
 
-    /**
-     * Tests the index action.
-     */
-    public function testIndexAction()
-    {
-        $request = new Request();
-        $request->setRequestFormat(self::FORMAT);
-        $data = [
-            'hello' => 'world!',
-        ];
-        $response = new Response(json_encode($data));
-
-        $serializer = $this->getSerializer();
-        $serializer->expects($this->once())
-                   ->method('respond')
-                   ->with($data, self::FORMAT)
-                   ->willReturn($response);
-
-        $default = new DefaultController($serializer, $this->getDoctrine(), $this->getTokenStorage());
-        $result = $default->indexAction($request);
-
-        $this->assertEquals($response, $result);
-    }
+    // /**
+    //  * Tests the index action.
+    //  */
+    // public function testIndexAction()
+    // {
+    //     $request = new Request();
+    //     $request->setRequestFormat(self::FORMAT);
+    //     $data = [
+    //         'hello' => 'world!',
+    //     ];
+    //     $response = new Response(json_encode($data));
+    //
+    //     $serializer = $this->getSerializer();
+    //     $serializer->expects($this->once())
+    //                ->method('respond')
+    //                ->with($data, self::FORMAT)
+    //                ->willReturn($response);
+    //
+    //     $default = new DefaultController($serializer, $this->getDoctrine(), $this->getTokenStorage());
+    //     $result = $default->indexAction($request);
+    //
+    //     $this->assertEquals($response, $result);
+    // }
 }
