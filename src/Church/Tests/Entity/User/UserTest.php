e7481bc9dd4be73e201fae8545a3b52b999e7e6a
David Barratt <david@thechur.ch>
2017-03-19 14:22:14 -0400

    Add neighbor test

--- src/Church/Tests/Entity/User/UserTest.php
+++ src/Church/Tests/Entity/User/UserTest.php
@@ -41,4 +41,52 @@ class UserTest extends EntityTest
             'standard',
         ], $roles);
     }
+
+    public function testIsNeighbor()
+    {
+        $user = new User();
+        $other = new User();
+
+        $this->assertFalse($user->isNeighbor($other));
+
+        $user = new User([
+            'name' => [
+                'first' => 'Test',
+                'last' => 'User',
+            ],
+            'primaryEmail' => [
+                'email' => 'test@example.com',
+                'verified' => new \DateTime()
+            ],
+            'orthodox' => true,
+            'username' => 'test',
+            'location' => [
+                'id' => '123',
+                'place' => [
+                    'id' => 874397665,
+                ],
+            ],
+        ]);
+
+        $other = $user = new User([
+            'name' => [
+                'first' => 'Other',
+                'last' => 'User',
+            ],
+            'primaryEmail' => [
+                'email' => 'other@example.com',
+                'verified' => new \DateTime()
+            ],
+            'orthodox' => true,
+            'username' => 'test',
+            'location' => [
+                'id' => '456',
+                'place' => [
+                    'id' => 874397665,
+                ],
+            ],
+        ]);
+
+        $this->assertTrue($user->isNeighbor($other));
+    }
 }
