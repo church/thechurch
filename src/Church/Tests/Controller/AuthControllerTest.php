cb883059008d8015023888222c61eff78d43c899
David Barratt <david@thechur.ch>
2017-03-26 15:49:35 -0400

    Add login email action test

--- src/Church/Tests/Controller/AuthControllerTest.php
+++ src/Church/Tests/Controller/AuthControllerTest.php
@@ -3,8 +3,13 @@
 namespace Church\Tests\Controller;
 
 use Church\Controller\AuthController;
+use Church\Entity\User\Email;
 use Church\Entity\User\Login;
+use Church\Entity\User\User;
+use Church\Entity\User\Verify\EmailVerify;
 use Church\Utils\User\VerificationManagerInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityRepository;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -34,7 +39,7 @@ class AuthControllerTest extends ControllerTest
             ->getMock();
         $request->expects($this->once())
             ->method('getRequestFormat')
-            ->willReturn('json');
+            ->willReturn('test');
 
         $login = $this->getMockBuilder(Login::class)
             ->disableOriginalConstructor()
@@ -46,6 +51,89 @@ class AuthControllerTest extends ControllerTest
             ->willReturn($login);
 
         $response = $controller->loginAction($request);
+
+        $this->assertInstanceOf(Response::class, $response);
+    }
+
+    public function testLoginEmailAction()
+    {
+        $serializer = $this->getSerializer();
+        $doctrine = $this->getDoctrine();
+        $verificationManager = $this->createMock(VerificationManagerInterface::class);
+        $jwtManager = $this->createMock(JWTManagerInterface::class);
+        $tokenStorage = $this->createMock(TokenStorageInterface::class);
+
+        $controller = new AuthController(
+            $serializer,
+            $doctrine,
+            $tokenStorage,
+            $verificationManager,
+            $jwtManager,
+            $tokenStorage
+        );
+
+        $request = $this->getMockBuilder(Request::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $request->expects($this->once())
+            ->method('getRequestFormat')
+            ->willReturn('test');
+
+        $token = 'abc';
+
+        $created = $this->getMockBuilder(\DateTime::class)
+            ->getMock();
+
+        $user = $this->getMockBuilder(User::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $email = $this->getMockBuilder(Email::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $email->expects($this->once())
+            ->method('getUser')
+            ->willReturn($user);
+
+        $verify = $this->getMockBuilder(EmailVerify::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $verify->expects($this->once())
+            ->method('getToken')
+            ->willReturn($token);
+        $verify->expects($this->once())
+            ->method('getCreated')
+            ->willReturn($created);
+        $verify->expects($this->once())
+            ->method('getEmail')
+            ->willReturn($email);
+
+        $serializer->expects($this->once())
+            ->method('request')
+            ->with($request, EmailVerify::class)
+            ->willReturn($verify);
+
+        $repository = $this->getMockBuilder(EntityRepository::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $repository->expects($this->once())
+            ->method('__call')
+            ->with('findOneByToken', [$token])
+            ->willReturn($verify);
+
+        $em = $this->getMockBuilder(ObjectManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $doctrine->expects($this->once())
+            ->method('getRepository')
+            ->with(EmailVerify::class)
+            ->willReturn($repository);
+        $doctrine->expects($this->once())
+            ->method('getEntityManager')
+            ->willReturn($em);
+
+        $response = $controller->loginEmailAction($request);
 
         $this->assertInstanceOf(Response::class, $response);
     }
